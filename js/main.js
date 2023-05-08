414410f4df7a9002a128a9863f066ea5200afa8f
TommasoPezzola <pezzola123@gmail.com>
2023-05-08 14:48:49 +0200

    Bonus

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      img: `https://www.freecodecamp.org/news/content/images/size/w2000/2022/08/Vue-Blog-Cover-2.png`
     }
   }
 }).mount('#app')
