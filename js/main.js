70d268e625d0ccd60b84c466f1b0968a1d13ce65
TommasoPezzola <pezzola123@gmail.com>
2023-05-08 16:49:57 +0200

    feat(js): add URL

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ createApp({
   data() {
     return {
       message: 'Hello Vue!',
-      img: `https://www.freecodecamp.org/news/content/images/size/w2000/2022/08/Vue-Blog-Cover-2.png`
+      img: ``
     }
   }
 }).mount('#app')
